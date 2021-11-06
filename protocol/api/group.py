d535d3e74982b06bc4e4a9f684bed6f0501451a9
DeadZeta <nikitos28022004@gmail.com>
2021-11-06 22:29:15 +0300

    v1.0.2

---
+++ protocol/api/group.py
@@ -0,0 +1,19 @@
+from protocol.api.base import user_send_api, group_send_api, rand
+from time import time as unixtime
+
+def send_message(peer_id: int, message: str):
+    return group_send_api('messages.send', {
+        'peer_id': peer_id,
+        'message': message,
+        'random_id': rand()
+    })
+
+def wall_post(owner_id: int, message: str,
+              comments: bool = True, publish_date: int = 0):
+    return user_send_api('wall.post', {
+        'owner_id': -owner_id,
+        'message': message,
+        'from_group': 1,
+        'close_comments': 1 if comments else 0,
+        'publish_date': int(unixtime()) + publish_date if publish_date != 0 else publish_date
+    })
